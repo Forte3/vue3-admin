92ebbbb4db97c3e69f35094fbb14f802f44646b9
Forte <lianghaizhao@icloud.com>
2022-10-23 23:02:20 +0800

    add Tabel.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,28 +1,28 @@
 <template>
-  <div class="header">
-    <div class="left">
-      <el-icon class="back" v-if="state.hasBack" @click="back">
-        <Back />
-      </el-icon>
-      <span style="font-size: 20px">{{ state.name }}</span>
-    </div>
-    <div class="right">
-      <el-popover placement="bottom" :width="320" trigger="click" popper-class="popper-user-box">
-        <template #reference>
-          <div class="author">
-            <i class="icon el-icon-s-custom" />
-            {{ state.userInfo && state.userInfo.nickName || '' }}
-            <i class="el-icon-caret-bottom" />
-          </div>
-        </template>
-        <div class="nickname">
-          <p>登录名：{{ state.userInfo && state.userInfo.loginUserName || '' }}</p>
-          <p>昵称：{{ state.userInfo && state.userInfo.nickName || '' }}</p>
-          <el-tag size="small" effect="dark" class="logout" @click="logout">退出</el-tag>
-        </div>
-      </el-popover>
-    </div>
-  </div>
+   <div class="header">
+      <div class="left">
+         <el-icon class="back" v-if="state.hasBack" @click="back">
+            <Back />
+         </el-icon>
+         <span style="font-size: 20px">{{ state.name }}</span>
+      </div>
+      <div class="right">
+         <el-popover placement="bottom" :width="320" trigger="click" popper-class="popper-user-box">
+            <template #reference>
+               <div class="author">
+                  <i class="icon el-icon-s-custom" />
+                  {{ state.userInfo && state.userInfo.nickName || '' }}
+                  <i class="el-icon-caret-bottom" />
+               </div>
+            </template>
+            <div class="nickname">
+               <p>登录名：{{ state.userInfo && state.userInfo.loginUserName || '' }}</p>
+               <p>昵称：{{ state.userInfo && state.userInfo.nickName || '' }}</p>
+               <el-tag size="small" effect="dark" class="logout" @click="logout">退出</el-tag>
+            </div>
+         </el-popover>
+      </div>
+   </div>
 </template>
 
 <script setup>
@@ -33,92 +33,92 @@ import axios from '@/utils/axios'
 
 const router = useRouter()
 const state = reactive({
-  name: 'dashboard',
-  userInfo: null, // 用户信息
-  hasBack: false // 是否显示返回图标
+   name: 'dashboard',
+   userInfo: null, // 用户信息
+   hasBack: false // 是否显示返回图标
 })
 
 onMounted(() => {
-  const pathname = window.location.hash.split('/')[1] || ''
-  if (!['login'].includes(pathname)) {
-    getUserInfo()
-  }
+   const pathname = window.location.hash.split('/')[1] || ''
+   if (!['login'].includes(pathname)) {
+      getUserInfo()
+   }
 })
 
 // 获取用户信息
 const getUserInfo = async () => {
-  const userInfo = await axios.get('/adminUser/profile')
-  state.userInfo = userInfo
+   const userInfo = await axios.get('/adminUser/profile')
+   state.userInfo = userInfo
 }
 
 // 退出登录
 const logout = () => {
-  axios.delete('/logout').then(() => {
-    // 退出之后，将本地保存的 token 删除
-    localRemove('token')
-    // 回到登录页
-    router.push({ path: '/login' })
-  })
+   axios.delete('/logout').then(() => {
+      // 退出之后，将本地保存的 token 删除
+      localRemove('token')
+      // 回到登录页
+      router.push({ path: '/login' })
+   })
 }
 
 router.afterEach((to) => {
-  // console.log('to', to)
-  const { id } = to.query
-  state.name = pathMap[to.name]
-  state.hasBack = ['level2', 'level3'].includes(to.name)
+   // console.log('to', to)
+   const { id } = to.query
+   state.name = pathMap[to.name]
+   state.hasBack = ['level2', 'level3'].includes(to.name)
 })
 
 const back = () => {
-  router.back()
+   router.back()
 }
 
 </script>
 
 <style scoped>
 .header {
-  height: 50px;
-  border-bottom: 1px solid #e9e9e9;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 0 20px;
+   height: 50px;
+   border-bottom: 1px solid #e9e9e9;
+   display: flex;
+   justify-content: space-between;
+   align-items: center;
+   padding: 0 20px;
 }
 
 .right>div>.icon {
-  font-size: 18px;
-  margin-right: 6px;
+   font-size: 18px;
+   margin-right: 6px;
 }
 
 .author {
-  margin-left: 10px;
-  cursor: pointer;
+   margin-left: 10px;
+   cursor: pointer;
 }
 </style>
 
 <style>
 .popper-user-box {
-  background: url('https://s.yezgea02.com/lingling-h5/static/account-banner-bg.png') 50% 50% no-repeat !important;
-  background-size: cover !important;
-  border-radius: 0 !important;
+   background: url('https://s.yezgea02.com/lingling-h5/static/account-banner-bg.png') 50% 50% no-repeat !important;
+   background-size: cover !important;
+   border-radius: 0 !important;
 }
 
 .popper-user-box .nickname {
-  position: relative;
-  color: #ffffff;
+   position: relative;
+   color: #ffffff;
 }
 
 .popper-user-box .nickname .logout {
-  position: absolute;
-  right: 0;
-  top: 0;
-  cursor: pointer;
+   position: absolute;
+   right: 0;
+   top: 0;
+   cursor: pointer;
 }
 
 .header .left .back {
-  border: 1px solid #e9e9e9;
-  padding: 5px;
-  border-radius: 50%;
-  margin-right: 5px;
-  cursor: pointer;
+   border: 1px solid #e9e9e9;
+   padding: 5px;
+   border-radius: 50%;
+   margin-right: 5px;
+   cursor: pointer;
 }
 </style>
--- src/views/test.vue
+++ src/views/test.vue
@@ -1,31 +1,31 @@
 <template>
-  <el-card class="category-container">
-    <el-table :load="state.loading" ref="multipleTable" :data="tableData" tooltip-effect="dark" style="width: 100%"
-      @selection-change="handleSelectionChange">
-      <el-table-column type="selection" width="55">
-      </el-table-column>
-      <el-table-column prop="categoryName" label="分类名称">
-      </el-table-column>
-      <el-table-column prop="categoryRank" label="排序值" width="120">
-      </el-table-column>
-      <el-table-column prop="createTime" label="添加时间" width="200">
-      </el-table-column>
-      <el-table-column label="操作" width="220">
-        <template #default="scope">
-          <a style="cursor: pointer; margin-right: 10px" @click="handleEdit(scope.row.categoryId)">修改</a>
-          <a style="cursor: pointer; margin-right: 10px" @click="handleNext(scope.row)">下级分类</a>
-          <el-popconfirm title="确定删除吗？" @confirm="handleDeleteOne(scope.row.categoryId)">
-            <template #reference>
-              <a style="cursor: pointer">删除</a>
+   <el-card class="category-container">
+      <el-table :load="state.loading" ref="multipleTable" :data="tableData" tooltip-effect="dark" style="width: 100%"
+         @selection-change="handleSelectionChange">
+         <el-table-column type="selection" width="55">
+         </el-table-column>
+         <el-table-column prop="categoryName" label="分类名称">
+         </el-table-column>
+         <el-table-column prop="categoryRank" label="排序值" width="120">
+         </el-table-column>
+         <el-table-column prop="createTime" label="添加时间" width="200">
+         </el-table-column>
+         <el-table-column label="操作" width="220">
+            <template #default="scope">
+               <a style="cursor: pointer; margin-right: 10px" @click="handleEdit(scope.row.categoryId)">修改</a>
+               <a style="cursor: pointer; margin-right: 10px" @click="handleNext(scope.row)">下级分类</a>
+               <el-popconfirm title="确定删除吗？" @confirm="handleDeleteOne(scope.row.categoryId)">
+                  <template #reference>
+                     <a style="cursor: pointer">删除</a>
+                  </template>
+               </el-popconfirm>
             </template>
-          </el-popconfirm>
-        </template>
-      </el-table-column>
-    </el-table>
-    <!--总数超过一页，再展示分页器-->
-    <el-pagination background layout="prev, pager, next" :total="state.total" :page-size="state.pageSize"
-      :current-page="state.currentPage" @current-change="changePage" />
-  </el-card>
+         </el-table-column>
+      </el-table>
+      <!--总数超过一页，再展示分页器-->
+      <el-pagination background layout="prev, pager, next" :total="state.total" :page-size="state.pageSize"
+         :current-page="state.currentPage" @current-change="changePage" />
+   </el-card>
 </template>
 
 <script setup>
@@ -35,96 +35,96 @@ import axios from '@/utils/axios'
 import { ElMessage } from 'element-plus'
 
 const props = defineProps({
-  type: String, // 用于判断是添加还是编辑
-  reload: Function // 添加或修改完后，刷新列表页
+   type: String, // 用于判断是添加还是编辑
+   reload: Function // 添加或修改完后，刷新列表页
 })
 
 const formRef = ref(null)
 const route = useRoute()
 const state = reactive({
-  visible: false,
-  categoryLevel: 1,
-  parentId: 0,
-  ruleForm: {
-    name: '',
-    rank: ''
-  },
-  rules: {
-    name: [
-      { required: 'true', message: '名称不能为空', trigger: ['change'] }
-    ],
-    rank: [
-      { required: 'true', message: '编号不能为空', trigger: ['change'] }
-    ]
-  },
-  id: ''
+   visible: false,
+   categoryLevel: 1,
+   parentId: 0,
+   ruleForm: {
+      name: '',
+      rank: ''
+   },
+   rules: {
+      name: [
+         { required: 'true', message: '名称不能为空', trigger: ['change'] }
+      ],
+      rank: [
+         { required: 'true', message: '编号不能为空', trigger: ['change'] }
+      ]
+   },
+   id: ''
 })
 // 获取详情
 const getDetail = (id) => {
-  axios.get(`/categories/${id}`).then(res => {
-    state.ruleForm = {
-      name: res.categoryName,
-      rank: res.categoryRank
-    }
-    state.parentId = res.parentId
-    state.categoryLevel = res.categoryLevel
-  })
+   axios.get(`/categories/${id}`).then(res => {
+      state.ruleForm = {
+         name: res.categoryName,
+         rank: res.categoryRank
+      }
+      state.parentId = res.parentId
+      state.categoryLevel = res.categoryLevel
+   })
 }
 // 开启弹窗
 const open = (id) => {
-  state.visible = true
-  if (id) {
-    state.id = id
-    // 如果是有 id 传入，证明是修改模式
-    getDetail(id)
-  } else {
-    // 否则为新增模式
-    // 新增类目，从路由获取分类 level 级别和父分类 id
-    const { level = 1, parent_id = 0 } = route.query
-    state.ruleForm = {
-      name: '',
-      rank: ''
-    }
-    state.parentId = parent_id
-    state.categoryLevel = level
-  }
+   state.visible = true
+   if (id) {
+      state.id = id
+      // 如果是有 id 传入，证明是修改模式
+      getDetail(id)
+   } else {
+      // 否则为新增模式
+      // 新增类目，从路由获取分类 level 级别和父分类 id
+      const { level = 1, parent_id = 0 } = route.query
+      state.ruleForm = {
+         name: '',
+         rank: ''
+      }
+      state.parentId = parent_id
+      state.categoryLevel = level
+   }
 }
 // 关闭弹窗
 const close = () => {
-  state.visible = false
+   state.visible = false
 }
 const submitForm = () => {
-  formRef.value.validate((valid) => {
-    if (valid) {
-      if (props.type == 'add') {
-        // 添加方法
-        axios.post('/categories', {
-          categoryLevel: state.categoryLevel, // 分类等级
-          parentId: state.parentId, // 当前分类的父分类 id
-          categoryName: state.ruleForm.name, // 分类名称
-          categoryRank: state.ruleForm.rank // 分类权重
-        }).then(() => {
-          ElMessage.success('添加成功')
-          state.visible = false
-          // 接口回调之后，运行重新获取列表方法 reload
-          if (props.reload) props.reload()
-        })
-      } else {
-        // 修改方法
-        axios.put('/categories', {
-          categoryId: state.id,
-          categoryLevel: state.categoryLevel,
-          parentId: state.categoryLevel,
-          categoryName: state.ruleForm.name,
-          categoryRank: state.ruleForm.rank
-        }).then(() => {
-          ElMessage.success('修改成功')
-          state.visible = false
-          // 接口回调之后，运行重新获取列表方法 reload
-          if (props.reload) props.reload()
-        })
+   formRef.value.validate((valid) => {
+      if (valid) {
+         if (props.type == 'add') {
+            // 添加方法
+            axios.post('/categories', {
+               categoryLevel: state.categoryLevel, // 分类等级
+               parentId: state.parentId, // 当前分类的父分类 id
+               categoryName: state.ruleForm.name, // 分类名称
+               categoryRank: state.ruleForm.rank // 分类权重
+            }).then(() => {
+               ElMessage.success('添加成功')
+               state.visible = false
+               // 接口回调之后，运行重新获取列表方法 reload
+               if (props.reload) props.reload()
+            })
+         } else {
+            // 修改方法
+            axios.put('/categories', {
+               categoryId: state.id,
+               categoryLevel: state.categoryLevel,
+               parentId: state.categoryLevel,
+               categoryName: state.ruleForm.name,
+               categoryRank: state.ruleForm.rank
+            }).then(() => {
+               ElMessage.success('修改成功')
+               state.visible = false
+               // 接口回调之后，运行重新获取列表方法 reload
+               if (props.reload) props.reload()
+            })
+         }
       }
-    }
-  })
+   })
 }
 </script>
